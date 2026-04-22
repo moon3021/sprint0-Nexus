3b5bc4506ec35f79aa72726133de4ea4850ade4f
moon3021 <gxz1213@qq.com>
2026-04-22 21:39:23 +0800

    fix: 开放模块给 JUnit 测试访问

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,11 @@ module com.iwillrecitewords {
     requires javafx.controls;
     requires javafx.fxml;
     opens com.iwillrecitewords to javafx.graphics;
-    // 开放页面包给FXML（如果后续用FXML）
     opens com.iwillrecitewords.view to javafx.fxml;
-    // 导出包
     exports com.iwillrecitewords;
     exports com.iwillrecitewords.view;
     exports com.iwillrecitewords.service;
     exports com.iwillrecitewords.model;
+
+    opens com.iwillrecitewords to org.junit.jupiter;
 }
